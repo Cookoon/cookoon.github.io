e29ecf432fd37fa659105de5f740587e3a1368be
Charles Pernet <charlespernet@gmail.com>
2018-01-22 19:23:34 +0100

    Add host member and warranties pages

---
+++ src/pages/host.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const Host = () => (
+  <div>
+    <h1>DEVENIR HÔTE</h1>
+    <p>Bienvenue sur la page Hôte</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
+
+export default Host;
