1a2e44664ec63d3d1349380fcd135705c8e16afb
Francois Catuhe <fcatuhe@gmail.com>
2018-01-22 12:45:30 +0100

    prettier on Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const Header = () => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      marginBottom: '1.45rem'
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '1.45rem 1.0875rem'
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -20,7 +20,7 @@ const Header = () => (
           to="/"
           style={{
             color: 'white',
-            textDecoration: 'none',
+            textDecoration: 'none'
           }}
         >
           Gatsby
@@ -28,6 +28,6 @@ const Header = () => (
       </h1>
     </div>
   </div>
-)
+);
 
-export default Header
+export default Header;
