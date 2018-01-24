cded40fe8f53bba579f21e5b53f514ed54e22a73
Francois Catuhe <fcatuhe@gmail.com>
2018-01-24 11:58:46 +0100

    wip flip-cards in pure css

--- src/pages/etre-membre.js
+++ src/pages/etre-membre.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
 
 import Banner from '../components/Banner';
+import iconUser from '../images/iconUser.png';
+import iconSnap from '../images/iconSnap.png';
+import iconJanitor from '../images/iconJanitor.png';
 
 const MemberPage = () => (
   <div>
@@ -16,6 +20,84 @@ const MemberPage = () => (
         Et vous, pourquoi allez-vous louer un Cookoon ?
       </p>
     </Banner>
+
+    <div className="container text-center">
+      <p className="mt-4 font-weight-bold">
+        Louer un Cookoon
+        <br />pour tous vos besoins personnels ou professionnels
+      </p>
+
+      <div className="row text-white">
+        <div className="col-md">
+          <div
+            className="flip-container"
+            ontouchstart="this.classList.toggle('hover');"
+          >
+            <div className="flipper">
+              <div className="front bg-primary rounded p-4 d-flex flex-column justify-content-around align-items-center">
+                <img src={iconUser} role="presentation" className="mb-4 w-6r" />
+                <h2 className="h3">Un repas hors les murs</h2>
+              </div>
+              <div className="back bg-dark rounded p-4 d-flex flex-column justify-content-around align-items-center">
+                <img src={iconUser} role="presentation" className="mb-4 w-6r" />
+                <p className="h3">Un repas hors les murs</p>
+                <p className="text-justify">
+                  Pour un déjeuner ou un repas entre amis, quand on manque de
+                  plance ou que l'on souhaite changer d'air, Cookon est idéal.
+                  <br />Ça l'est encore plus avec un chef à domicile !
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md">
+          <div
+            className="flip-container"
+            ontouchstart="this.classList.toggle('hover');"
+          >
+            <div className="flipper">
+              <div className="front bg-primary rounded p-4 d-flex flex-column justify-content-around align-items-center">
+                <img src={iconUser} role="presentation" className="mb-4 w-6r" />
+                <h2 className="h3">Un repas hors les murs</h2>
+              </div>
+              <div className="back bg-dark rounded p-4 d-flex flex-column justify-content-around align-items-center">
+                <img src={iconUser} role="presentation" className="mb-4 w-6r" />
+                <p className="h3">Un repas hors les murs</p>
+                <p className="text-justify">
+                  Pour un déjeuner ou un repas entre amis, quand on manque de
+                  plance ou que l'on souhaite changer d'air, Cookon est idéal.
+                  <br />Ça l'est encore plus avec un chef à domicile !
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md">
+          <div
+            className="flip-container"
+            ontouchstart="this.classList.toggle('hover');"
+          >
+            <div className="flipper">
+              <div className="front bg-primary rounded p-4 d-flex flex-column justify-content-around align-items-center">
+                <img src={iconUser} role="presentation" className="mb-4 w-6r" />
+                <h2 className="h3">Un repas hors les murs</h2>
+              </div>
+              <div className="back bg-dark rounded p-4 d-flex flex-column justify-content-around align-items-center">
+                <img src={iconUser} role="presentation" className="mb-4 w-6r" />
+                <p className="h3">Un repas hors les murs</p>
+                <p className="text-justify">
+                  Pour un déjeuner ou un repas entre amis, quand on manque de
+                  plance ou que l'on souhaite changer d'air, Cookon est idéal.
+                  <br />Ça l'est encore plus avec un chef à domicile !
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
