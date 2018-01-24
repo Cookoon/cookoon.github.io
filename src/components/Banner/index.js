660e42565e3f3df2fc6d8e8ed5912d648c80428b
Charles Pernet <charlespernet@gmail.com>
2018-01-24 16:46:55 +0100

    Fix .device-frame size on small devices

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -14,10 +14,9 @@ const Banner = ({ children }) => (
       <div className="row h-100 align-items-center">
         <div className="col-md-7 offset-lg-1 col-lg-6 offset-xl-2 col-xl-5 text-center text-md-left">
           <img alt="Cookoon" src={logo} className="mb-4" />
-
           {children}
         </div>
-        <div className="col-md-5 col-lg-4 offset-xl-1 col-xl-3">
+        <div className="col-md-5 col-lg-4 offset-xl-1 col-xl-3 d-flex justify-content-center">
           <DeviceFrame>
             <img alt="Application Cookoon" src={appScreen} />
           </DeviceFrame>
