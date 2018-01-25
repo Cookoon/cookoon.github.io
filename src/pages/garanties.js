c85e04e9c857e97a914f922f29ba4fbdb4612a5e
Francois Catuhe <fcatuhe@gmail.com>
2018-01-25 15:41:34 +0100

    finish garanties banner

--- src/pages/garanties.js
+++ src/pages/garanties.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 import Banner from '../components/Banner';
+import logoAxa from '../images/logoAxa.png';
 
 const InsurancePage = () => (
   <div>
     <Helmet title="Garanties" />
 
     <Banner>
-      <h1 className="h4 font-weight-light mb-4">Garanties</h1>
+      <h1 className="h4 font-weight-light mb-4">
+        Votre assurance incluse à chaque location
+      </h1>
+      <div className="d-flex justify-content-center justify-content-md-start align-items-end">
+        <p className="small mb-2 mr-2">en partenariat avec </p>
+        <img src={logoAxa} alt="Axa" className="w-6r" />
+      </div>
     </Banner>
   </div>
 );
