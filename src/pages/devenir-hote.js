bc00498967b51a3568c24a5aec19d4ec52dcd5e5
Charles Pernet <charlespernet@gmail.com>
2018-01-25 16:54:20 +0100

    Add links on host_page

--- src/pages/devenir-hote.js
+++ src/pages/devenir-hote.js
@@ -3,9 +3,6 @@ import Link from 'gatsby-link';
 
 import Helmet from 'react-helmet';
 import Banner from '../components/Banner';
-import iconUser from '../images/iconUser.png';
-import iconSnap from '../images/iconSnap.png';
-import iconJanitor from '../images/iconJanitor.png';
 
 const HostPage = () => (
   <div>
@@ -16,9 +13,14 @@ const HostPage = () => (
         Rentabilisez votre logement pendant vos absences
         quotidiennes, et sans contraintes avec les services.
       </h1>
-      <button className="btn btn-lg bg-white px-5 mb-4">
-        DEVENIR HÔTE
-      </button>
+      <a
+        href="https://quentin21.typeform.com/to/ljnp8X"
+        className="btn btn-md bg-white px-5 mb-4"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        POSTULER
+      </a>
     </Banner>
 
     <div className="container text-center">
@@ -29,9 +31,11 @@ const HostPage = () => (
       </p>
 
       <div className="row">
-        <div className="col-md p-4 d-flex flex-column justify-content-around align-items-center">
-          <img src={iconUser} role="presentation" className="mb-4 w-6r" />
-          <h2 className="h3 font-weight-normal mb-4">Un appartement</h2>
+        <div className="col-md px-4 d-flex flex-column justify-content-between align-items-center">
+          <div>
+            <i className="co co-home text-primary fa-5x" aria-hidden="true" />
+            <h2 className="h3 font-weight-normal my-4">Un appartement</h2>
+          </div>
           <p className="text-justify">
             Les Hôtes inscrivent leur logement sur l’application Cookoon,
             le rendent disponible durant leurs absences quotidiennes et reçoivent le paiement
@@ -41,12 +45,14 @@ const HostPage = () => (
             Le tarif horaire de location est fixé par l’Hôte, que nous conseillons en fonction de sa taille,
             de son emplacement, ou encore de la demande des membres.
           </p>
-          <Link to="/etre-membre/">En savoir plus ></Link>
+          <div className='mb-3'></div>
         </div>
 
-        <div className="col-md p-4 d-flex flex-column justify-content-around align-items-center">
-          <img src={iconSnap} role="presentation" className="mb-4 w-6r" />
-          <h2 className="h3 font-weight-normal mb-4">Le rentabiliser simplement</h2>
+        <div className="col-md px-4 d-flex flex-column justify-content-between align-items-center">
+          <div>
+            <i className="co co-success text-primary fa-5x" aria-hidden="true" />
+            <h2 className="h3 font-weight-normal my-4">Le rentabiliser simplement</h2>
+          </div>
           <p className="text-justify">
             Avec seulement 2 locations de quelques heures par semaine, les membres peuvent obtenir jusqu'à 1500 €
             par mois en fonction du tarif horaire qu'ils proposent.
@@ -55,12 +61,14 @@ const HostPage = () => (
             Cookoon prélève 7% au titre des frais de services, et vous propose des options supplémentaires :
             Conciergerie et Ménage à partir de 20€
           </p>
-          <Link to="/">En savoir plus ></Link>
+          <div className='mb-3'></div>
         </div>
 
-        <div className="col-md p-4 d-flex flex-column justify-content-around align-items-center">
-          <img src={iconJanitor} role="presentation" className="mb-4 w-6r" />
-          <h2 className="h3 font-weight-normal mb-4">En toute tranquilité</h2>
+        <div className="col-md px-4 pb-4 d-flex flex-column justify-content-between align-items-center">
+          <div>
+            <i className="co co-concierge text-primary fa-5x" aria-hidden="true" />
+            <h2 className="h3 font-weight-normal my-4">En toute tranquilité</h2>
+          </div>
           <p className="text-justify">
             Seuls les membres peuvent louer l’appartement de l’Hôte.
           </p>
@@ -71,37 +79,37 @@ const HostPage = () => (
             AXA, notre partenaire, garantit toutes les locations via un contrat d’assurance
             spécialement conçu pour les usages Cookoon.
           </p>
-          <Link to="/devenir-hote/">En savoir plus ></Link>
+          <Link to="/garanties/">En savoir plus ></Link>
         </div>
       </div>
     </div>
     <div className="bg-white py-5">
       <div className="container">
         <div className="row">
-          <div className="col-md">
+          <div className="col-md d-flex flex-column justify-content-around">
             <h3>Qui sommes-nous ?</h3>
             <p>Cookoon est une communauté exclusive qui repose sur la confiance et l’implication de ses membres.</p>
             <p>Grâce à l’application Cookoon, les membres interagissent et accèdent à un certain nombre de services.</p>
           </div>
           <div className="col-md">
-            <div className="d-flex justify-content-beetween align-items-center">
-              <img src={iconJanitor} role="presentation" />
-              <p>
+            <div className="d-flex flex-column flex-md-row justify-content-beetween align-items-center">
+              <i className="co co-concierge text-primary mr-0 mr-md-3 fa-3x" aria-hidden="true" />
+              <p className='mt-2 mb-md-0 '>
                 Organiser des <strong>micro-événements du quotidien</strong> en louant un Cookoon et en choisissant des services
                 complémentaires comme un traiteur, un chef à domicile, un musicien, …
                </p>
             </div>
-            <div className="d-flex justify-content-beetween align-items-center">
-              <img src={iconJanitor} role="presentation" />
-              <p>
+            <div className="d-flex flex-column flex-md-row justify-content-beetween align-items-center">
+              <i className="co co-notifications text-primary mr-0 mr-md-3 fa-3x" aria-hidden="true" />
+              <p className='mt-2 mb-md-0 '>
                 Pour des <strong>usages personnels</strong> : un diner entre amis, un match de foot sur grand écran,
                 un vide-dressing etc., ou <strong>professionnels</strong> : un comité d’entreprise, un repas d’affaires,
                 une journée de formation, etc.
               </p>
             </div>
-            <div className="d-flex justify-content-beetween align-items-center">
-              <img src={iconJanitor} role="presentation" />
-              <p>
+            <div className="d-flex flex-column flex-md-row justify-content-beetween align-items-center">
+              <i className="co co-success text-primary mr-0 mr-md-3 fa-3x" aria-hidden="true" />
+              <p className='mt-2 mb-md-0 '>
                 <strong>Pour nous rejoindre ?</strong> Les membres doivent être cooptés par un autre membre disposant d’invitations.
                 Les invitations viennent récompenser l’implication des membres et l’usage respectueux des services.
               </p>
@@ -118,7 +126,14 @@ const HostPage = () => (
           logement : appartement, maison, loft ou château. S’il est retenu vous serez invité à nous rejoindre.
         </p>
       </div>
-      <button className="btn btn-lg bg-white px-5">DEVENIR HÔTE</button>
+      <a
+        href="https://quentin21.typeform.com/to/ljnp8X"
+        className="btn btn-md bg-white px-5 mb-4"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        POSTULER
+      </a>
     </div>
   </div>
 );
