6d8e9f83f52ed96bef4c855efcf1e57940bf5ac7
Charles Pernet <charlespernet@gmail.com>
2018-01-29 16:29:36 +0100

    Add .cookon-nav-simple for simpler navBar on some pages

--- src/pages/conditions-generales.js
+++ src/pages/conditions-generales.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+import React from "react";
+import Helmet from "react-helmet";
 
 const TermsPage = () => (
   <div>
     <Helmet title="Conditions générales" />
-
-    <div className="bg-primary h-7r" />
 
     <div className="container pt-4">
       <h3>Prologue</h3>
