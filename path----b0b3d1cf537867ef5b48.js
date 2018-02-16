888b3e7fdcb4aac28ba26b8a825ceeee80d1d134
Francois Catuhe <fcatuhe@gmail.com>
2018-02-16 17:16:51 +0100

    Updates

--- path----b0b3d1cf537867ef5b48.js
+++ path----b0b3d1cf537867ef5b48.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{94:function(o,e){o.exports={data:{site:{siteMetadata:{name:"Cookoon",title:"Recevons partout !",description:"Une communauté exclusive pour recevoir et partager",keywords:"cookoon, recevons, partout, communauté, exclusive, recevoir, partager",siteUrl:"https://cookoon.fr",twitter:"@cookoon"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{108:function(o,e){o.exports={data:{site:{siteMetadata:{name:"Cookoon",title:"Recevons partout !",description:"Une communauté exclusive pour recevoir et partager",keywords:"cookoon, recevons, partout, communauté, exclusive, recevoir, partager",siteUrl:"https://cookoon.fr",twitter:"@cookoon"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----b0b3d1cf537867ef5b48.js.map
