295bf8d5446885068784713f9e8f1f5aea541b8a
Francois Catuhe <fcatuhe@gmail.com>
2018-03-05 22:46:00 +0100

    update white paper

--- src/pages/livre-blanc.js
+++ src/pages/livre-blanc.js
@@ -9,7 +9,7 @@ const WhitePaperPage = () => (
     <div className="container">
       <iframe
         className="white-paper mt-3 mb-3"
-        src="https://drive.google.com/file/d/1CM1ZdA3OizqfrLn6Tt-vGS-Bmd-S_YFQ/preview"
+        src="https://drive.google.com/file/d/11h2Ox7iVJ7rWh9xEy4FezA6k2A2Ux34m/preview"
       />
     </div>
   </div>
