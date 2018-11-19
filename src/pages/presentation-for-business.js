5742f0ba5eb7eb5465238aa7dda64ea9f6d249cd
Charles Pernet <charlespernet@gmail.com>
2018-11-19 18:04:51 +0100

    Change url for presentation-business pdf

--- src/pages/presentation-for-business.js
+++ src/pages/presentation-for-business.js
@@ -9,7 +9,7 @@ const BusinessPresentationPage = () => (
     <div className="container">
       <iframe
         className="white-paper mt-3 mb-3"
-        src="https://drive.google.com/file/d/1Wll315GYu-4lkiHThLSmRxPfnxzL2emB/preview"
+        src="https://drive.google.com/file/d/1C1h5YUe6LQUIs5iue5rzVYZ1-CTehGXk/preview"
         frameBorder="0"
       />
     </div>
