0ea497f6efe4cae4b449c4da8c79f421018a530a
Charles Pernet <charlespernet@gmail.com>
2018-11-20 10:55:09 +0100

    Change business-presentation pdf link once again

--- src/pages/presentation-for-business.js
+++ src/pages/presentation-for-business.js
@@ -9,7 +9,7 @@ const BusinessPresentationPage = () => (
     <div className="container">
       <iframe
         className="white-paper mt-3 mb-3"
-        src="https://drive.google.com/file/d/1C1h5YUe6LQUIs5iue5rzVYZ1-CTehGXk/preview"
+        src="https://drive.google.com/file/d/1sp3XF4zlZL3FvhBwLx6nh7H0bwXh6ROL/preview"
         frameBorder="0"
       />
     </div>
