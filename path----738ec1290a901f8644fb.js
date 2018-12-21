0010f8ffa6381fc2f3eed7775850c666d16296b0
Charles Pernet <charlespernet@gmail.com>
2018-12-21 15:59:23 +0100

    Updates

---
+++ path----738ec1290a901f8644fb.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{107:function(o,e){o.exports={data:{site:{siteMetadata:{name:"Cookoon",title:"Recevons partout !",description:"Vos repas d’exception chez un particulier",fbAppId:"186829152103890",keywords:"cookoon, recevons, partout, communauté, communaute, sélective,\n    selective, recevoir, partager",siteUrl:"https://cookoon.fr",twitter:"@cookoon"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----738ec1290a901f8644fb.js.map
