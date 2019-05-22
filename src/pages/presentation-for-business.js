2cb574f37bda3b45fdd35d498f6cca2b5f151109
Charles Pernet <charlespernet@gmail.com>
2019-05-22 11:22:00 +0200

    change business presentation

--- src/pages/presentation-for-business.js
+++ src/pages/presentation-for-business.js
@@ -9,7 +9,7 @@ const BusinessPresentationPage = () => (
     <div className="container">
       <iframe
         className="white-paper mt-3 mb-3"
-        src="https://drive.google.com/file/d/1UhDkre-9Tqja53LpnpucDNHYG706Oev3/preview"
+        src="https://drive.google.com/file/d/1k8rzKX-yjMAi7lwRAOW4YCFCOMgv_dum/preview"
         frameBorder="0"
       />
     </div>
