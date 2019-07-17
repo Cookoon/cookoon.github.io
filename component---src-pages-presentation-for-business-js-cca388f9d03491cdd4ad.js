d788ee77af277ac0c9b05f5174dd366e9c5fee42
Charles Pernet <charlespernet@gmail.com>
2019-07-17 12:08:08 +0200

    Updates

---
+++ component---src-pages-presentation-for-business-js-cca388f9d03491cdd4ad.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{192:function(e,a,t){"use strict";t.r(a);var r=t(0),n=t.n(r),i=t(195),c=t.n(i),s=t(196);a.default=function(){return n.a.createElement(s.a,null,n.a.createElement(c.a,{title:"For Business"}),n.a.createElement("div",{className:"container"},n.a.createElement("iframe",{className:"white-paper mt-3 mb-3",src:"https://drive.google.com/file/d/1k8rzKX-yjMAi7lwRAOW4YCFCOMgv_dum/preview",frameBorder:"0"})))}}}]);
+//# sourceMappingURL=component---src-pages-presentation-for-business-js-cca388f9d03491cdd4ad.js.map
